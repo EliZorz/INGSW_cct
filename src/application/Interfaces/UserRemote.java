694a8a0329f40069fd462fde5c628a1085cb8971
LudovicaBeati <ludovicamaria.beati@gmail.com>
2018-03-20 12:15:10 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/application/Main.java
    #	src/application/MenuIniziale.fxml
    #	src/application/MenuInizialeController.java

--- src/application/Interfaces/UserRemote.java
+++ src/application/Interfaces/UserRemote.java
@@ -1,16 +1,18 @@
 package application.Interfaces;
 
-import netscape.javascript.JSObject;
+
+
+import org.json.simple.JSONObject;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface UserRemote extends Remote {
-    JSObject handleLogin() throws RemoteException; //questa funzione controllerà se username e password sono corrtti
-    JSObject logOut() throws RemoteException; //gestisce il bottone quando decido di fare il logout
-    JSObject save() throws RemoteException; //per il bottone save per quando si fanno dei cambiamenti
-    JSObject add() throws RemoteException; //per aggiungere bambini, fornitori, gite, staff
-    JSObject delete() throws RemoteException; //per eliminare quello detto prima
-    JSObject update() throws RemoteException; //per modificare quello detto prima
+    JSONObject handleLogin() throws RemoteException; //questa funzione controllerà se username e password sono corrtti
+    JSONObject logOut() throws RemoteException; //gestisce il bottone quando decido di fare il logout
+    JSONObject save() throws RemoteException; //per il bottone save per quando si fanno dei cambiamenti
+    JSONObject add() throws RemoteException; //per aggiungere bambini, fornitori, gite, staff
+    JSONObject delete() throws RemoteException; //per eliminare quello detto prima
+    JSONObject update() throws RemoteException; //per modificare quello detto prima
 
 }
