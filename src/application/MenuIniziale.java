aeb49132b5a445254aafb5f5c97bfccae7caa252
LudovicaBeati <ludovicamaria.beati@gmail.com>
2018-03-20 09:14:09 +0100

    commit

--- src/application/MenuIniziale.java
+++ src/application/MenuIniziale.java
@@ -4,13 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
 
 
 public class MenuIniziale extends Application {
