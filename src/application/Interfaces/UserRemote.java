5d93e9168c0603115268aa218f72542dc3b6fc9f
EliZorz <elisa1996>
2018-03-25 22:52:02 +0200

    commit new

--- src/application/Interfaces/UserRemote.java
+++ src/application/Interfaces/UserRemote.java
@@ -1,18 +1,14 @@
 package application.Interfaces;
 
-
-
-import org.json.simple.JSONObject;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface UserRemote extends Remote {
-    JSONObject handleLogin() throws RemoteException; //questa funzione controllerà se username e password sono corrtti
-    JSONObject logOut() throws RemoteException; //gestisce il bottone quando decido di fare il logout
-    JSONObject save() throws RemoteException; //per il bottone save per quando si fanno dei cambiamenti
-    JSONObject add() throws RemoteException; //per aggiungere bambini, fornitori, gite, staff
-    JSONObject delete() throws RemoteException; //per eliminare quello detto prima
-    JSONObject update() throws RemoteException; //per modificare quello detto prima
+    //boolean handleLogin() throws RemoteException; //questa funzione controllerà se username e password sono corrtti
+    void logOut() throws RemoteException; //gestisce il bottone quando decido di fare il logout
+    void save() throws RemoteException; //per il bottone save per quando si fanno dei cambiamenti
+    void add() throws RemoteException; //per aggiungere bambini, fornitori, gite, staff
+    void delete() throws RemoteException; //per eliminare quello detto prima
+    void update() throws RemoteException; //per modificare quello detto prima
 
 }
