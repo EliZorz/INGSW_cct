3397c046fc0fe5ab3c338caca378c9fb6cbaf2bc
LudovicaBeati <ludovicamaria.beati@gmail.com>
2018-03-27 18:44:52 +0200

    Socket connection

---
+++ src/application/socket/ServerSocket.java
@@ -0,0 +1,37 @@
+package application.socket;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ServerSocket {
+    public static void main(String args[]) {
+
+
+        Socket s = null;
+        java.net.ServerSocket ss2 = null;
+
+        try {
+            ss2 = new java.net.ServerSocket(4445); // creo un SocketThread con porta d'ascolto 4445
+            System.out.println("Server connection ready");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Server error");
+
+        }
+
+        while (true) {
+            try {
+                s = ss2.accept();  //gestione i vari client
+                System.out.println("connection Established");
+                SocketThread st = new SocketThread(s); // crea un thread per la gestione dei vari client
+                st.start();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Connection Error");
+
+            }
+        }
+    }
+}
