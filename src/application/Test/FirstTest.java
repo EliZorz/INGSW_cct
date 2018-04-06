b144502e66b72732c76d5dd261d42a10981ebee2
LudovicaBeati <ludovicamaria.beati@gmail.com>
2018-04-06 14:15:18 +0200

    test

---
+++ src/application/Test/FirstTest.java
@@ -0,0 +1,25 @@
+package application.Test;
+
+import application.rmi.server.ServerImpl;
+import org.junit.jupiter.api.Test;
+
+import java.rmi.RemoteException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest  {
+
+
+    //QUESTO TEST VERIFICA CHE NON RIESCA AD ACCEDERE AL DATABASE UN UTENTE CHE NON SCRIVE PASSWORD E USERNAME OPPURE NON NE SCRIVE UNO DEI DUE
+    @Test
+    public void testAdd1Plus() throws RemoteException {
+
+        ServerImpl si = new ServerImpl();
+
+        assertEquals(false, si.funzLog(null,null));
+        assertEquals(false, si.funzLog("ludo",null));
+        assertEquals(false, si.funzLog(null,"ludo123"));
+    }
+
+
+}
