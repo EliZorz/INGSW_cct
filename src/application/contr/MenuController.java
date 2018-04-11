890d51bd3247357a73deb8ebd145e5ed507c71c2
LudovicaBeati <ludovicamaria.beati@gmail.com>
2018-04-11 20:18:56 +0200

    last commit

---
+++ src/application/contr/MenuController.java
@@ -0,0 +1,40 @@
+package application.contr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+
+public class MenuController {
+
+    @FXML
+    TableView<String> table;
+
+    @FXML
+    private Button button;
+
+
+
+
+
+
+    public void pop(MouseEvent mouseEvent) {
+
+    }
+}
+
