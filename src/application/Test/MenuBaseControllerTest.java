da7fd7630a2b913aca65974120ddcdfdd1912bae
LudovicaBeati <ludovicamaria.beati@gmail.com>
2018-05-23 16:30:24 +0200

    test

--- src/application/Test/MenuBaseControllerTest.java
+++ src/application/Test/MenuBaseControllerTest.java
@@ -41,14 +41,78 @@ public class MenuBaseControllerTest {
     }
 
     @Test
+    void testSearchIngredients() throws RemoteException{
+        assertNotNull(si.searchIngredients("WWW"));
+    }
+
+    @Test
+    void testNullSearchIngredients() throws RemoteException {
+        assertNull(si.searchIngredients(null));
+    }
+
+    @Test
+    void testAddNullIngredientsDish(){
+        Assertions.assertThrows(NullPointerException.class, () ->{
+            si.saveIngredients(null, null);
+        });
+    }
+
+    @Test
+    void testAddNullSelectedIngredientsDish() {
+        Assertions.assertThrows(NullPointerException.class, () ->{
+            si.saveIngredients("AAA", null);
+        });
+    }
+
+    @Test
     void testAddMenu() throws RemoteException{
         assertTrue(si.addMenu("11", "aa", "bb", "cc", "WWW", "ee", LocalDate.parse("2200-11-11")));
     }
 
+    @Test
+    void testNullAddMenu(){
+        Assertions.assertThrows(NullPointerException.class, () ->{
+            si.addMenu(null, null, null, null, null, null, null);
+        });
+    }
+
+    @Test
+    void testLoadMenu() throws RemoteException{
+        assertNotNull(si.loadMenu());
+    }
+
+    @Test
+    void testControllData() throws RemoteException{
+        assertTrue(si.controllDate(LocalDate.parse("2200-11-11")));
+    }
+
+    @Test
+    void testNullControllData() throws RemoteException{
+        assertFalse(si.controllDate(LocalDate.parse("2222-11-11")));
+    }
+
+    @Test
+    void testUpdatMenu() throws RemoteException{
+        assertTrue(si.updateMenu("11","bb", "aa", "WWW", "cc", "ee", LocalDate.parse("2200-12-11"),LocalDate.parse("2200-11-11") ));
+    }
+
+    @Test
+    void testNullUpdate(){
+        Assertions.assertThrows(NullPointerException.class,()->{
+            si.updateMenu(null, null, null, null, null, null,LocalDate.parse(null), LocalDate.parse(null));
+        });
+    }
 
     @Test
     void testDeleteMenu() throws RemoteException{
         assertTrue(si.deleteMenu(LocalDate.parse("2200-11-11")));
+    }
+
+    @Test
+    void testNullDeleteMenu(){
+        Assertions.assertThrows(NullPointerException.class,()->{
+            si.deleteMenu(LocalDate.parse(null));
+        });
     }
 
     @Test
